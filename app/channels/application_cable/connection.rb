7e9d2f84d5cb9f97e7ba8179055df49b3936f2e4
Georgy Buranov <gburanov@gmail.com>
2016-08-10 20:31:52 +0200

    add rubocop

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,17 +1,18 @@
+# frozen_string_literal: true
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :uuid
 
     def connect
-      puts "New connection"
+      puts 'New connection'
       self.uuid = obtain_uuid
     end
 
     def obtain_uuid
-      uuid = cookies[:hacked_uuid]
-      reject_unauthorized_connection if uuid.blank?
-      return uuid
+      cookies[:hacked_uuid].tap do |uuid|
+        reject_unauthorized_connection if uuid.blank?
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
   def cors_preflight_check
@@ -23,5 +24,4 @@ class ApplicationController < ActionController::Base
       render :text => '', :content_type => 'text/plain'
     end
   end
-
 end
